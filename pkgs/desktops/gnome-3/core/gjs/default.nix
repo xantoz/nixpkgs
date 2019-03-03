b266e533d684fcef08318acbbc4cbf754e0ac9bc
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:11:28 +0100

    gnome3.gjs: 1.54.3 -> 1.55.92

--- pkgs/desktops/gnome-3/core/gjs/default.nix
+++ pkgs/desktops/gnome-3/core/gjs/default.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "gjs-${version}";
-  version = "1.54.3";
+  version = "1.55.92";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1cd65d4nq5xxlyjz1b83hm5zklyry6lillzf782nr0z97k60vcvn";
+    sha256 = "1iglzlkg2rczlsdaqgmiakqcr4aph98cnrm7lka1v54wivh94jfd";
   };
 
   passthru = {
